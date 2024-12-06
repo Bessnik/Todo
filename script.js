7ddf91b685d5c7ebafc1ea285ed14b9cdf639644
Besnik Mehmeti <bmehmeti@hotmail.fr>
2024-12-06 16:05:25 +0100

    Terminé todolist

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 const addBtn = document.querySelector(`#btn`);
 const taskCard = document.querySelector(".todoCard");
 const tasksContainer = document.querySelector("#todoCards");
+const delBtn = document.querySelector(`.delBtn`);
 const counter = document.querySelector("#count"); // on 
 
 // const addBtn = document.querySelector(`#btn`);
 addBtn.addEventListener(`click`, addTask); // add a task on click
 
+delBtn.addEventListener(`click`, function() { //delete default task on click
+    deleteTask(taskCard); // target the right task
+});
 
 function addTask() {
     const newTask = taskCard.cloneNode(true) // clone the task card
@@ -20,15 +24,16 @@ function addTask() {
     
     tasksContainer.appendChild(newTask) // append new task to the tasks container
     // updateCount();
+    counter.innerHTML = countCards(tasksContainer); // modifier le contenu de la div de qui à comme id "count" grace à la fonction 'countCards'
+}
+
+function countCards(listCard) { // Count le nombre de cards dans un list qui contient toutes les cards aui on dans la class 'todoCard'
+    let allCards = listCard.querySelectorAll(".todoCard"); // on prendre toutes le cards qui comme class 'todoCard' dans la div parent "todoCards"
+    return allCards.length;     // il count les cards qu'ils sont dans le div 'todoCard'
 }
 
 
-
-const delBtn = document.querySelector(`.delBtn`);
-delBtn.addEventListener(`click`, function() { //delete default task on click
-    deleteTask(taskCard); // target the right task
-});
-
 function deleteTask(task) {
     task.remove(); //remove the task
+    counter.innerHTML = countCards(tasksContainer); // modifier le contenu de la div de qui à comme id "count" grace à la fonction 'countCards'
 }
